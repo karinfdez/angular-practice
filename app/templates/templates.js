c2b51f610c77fc7f2a3a5c4bb3e6b2b704efe12e
Karin Fernandez <karinfdez@yahoo.com>
2016-03-18 15:48:16 -0400

    Added paypal form to each product and fixed list of products

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -20,9 +20,17 @@ angular.module('store.templates',['ngRoute'])
 	 
 	$http.get("json/templates.json").success(function(response){
 		$scope.templates=response;
-		console.log(response);
+		// console.log(response);
 	});
 }])
-.controller('TemplateDetailsCtrl', ['$scope',function($scope){
-	
+.controller('TemplateDetailsCtrl', ['$scope','$routeParams','$http','$filter',function($scope,$routeParams,$http,$filter){
+	// In order to get the id of the template.
+	var templateId=$routeParams.id;
+	$http.get("json/templates.json").success(function(response){
+		// Here I get the object with that specific ID.
+		$scope.template= $filter('filter')(response,function(data){
+			return data.id == templateId;
+		})[0];
+		$scope.mainImage=$scope.template.images[0].name;
+	});
 }]);
